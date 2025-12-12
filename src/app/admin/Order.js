b5529187a2bcaa8e7fee294bb45ad513f07bf5bc
mulen <mulen@mulens-MacBook-Air.local>
2025-12-12 12:24:07 +0800

    second

--- src/app/admin/Order.js
+++ src/app/admin/Order.js
@@ -2,5 +2,5 @@ export function Order({ setSelectedMenu }) {
   const handleMenuSelect = (menu) => {
     setSelectedMenu(menu);
   };
-  return <div className="text-white">hello</div>;
+  return <div className="w-[1440px] h-full bg-white text-black">hello</div>;
 }
